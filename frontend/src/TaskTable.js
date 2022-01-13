20241e9d5eff3b9d4e0670b4cc554e6e3f26f0a1
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2022-01-13 12:01:53 +0100

    Fixed bug causing infinite API calls

--- frontend/src/TaskTable.js
+++ frontend/src/TaskTable.js
@@ -12,23 +12,25 @@ const TaskTable = (props) => {
     fetch(URL)
       .then((res) => res.json())
       .then((json) => {
-          if (!_.isEqual(taskData, json)) {
-            if (props.where && props.week_number) {
-              setTaskData(json.filter((el) => {
-                return el.where == props.where &&
-                        el.week_number == props.week_number;
-              }));
-            }
-            else if (props.where) {
-              setTaskData(json.filter((el) => {
-                return el.where == props.where;
-              }));
-            }
-            else if (props.week_number) {
-              setTaskData(json.filter((el) => {
-                return el.week_number == props.week_number;
-              }));
-            }
+          var filtered_array = json;
+          if (props.where && props.week_number) {
+            filtered_array = json.filter( el => {
+              return el.where === props.where &&
+                      el.week_number === props.week_number;
+            });
+          }
+          else if (props.where) {
+            filtered_array = json.filter( el => {
+              return el.where === props.where;
+            });
+          }
+          else if (props.week_number) {
+            filtered_array = json.filter( el => {
+              return el.week_number === props.week_number;
+            });
+          }
+          if (!_.isEqual(taskData, filtered_array)) {
+            setTaskData(filtered_array);
           }
         })
   });
