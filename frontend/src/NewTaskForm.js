6d0c584fd4fbf9a8cb9c4026c9c0d4a062994132
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2022-01-18 14:25:14 +0100

    Barebones form working on newtaskform

--- frontend/src/NewTaskForm.js
+++ frontend/src/NewTaskForm.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewTaskForm = (props) => {
+  const [formData, setFormData] = useState({
+    text: ''
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  }
+
   return (
     <div>
       <h1>New Task Form Placeholder</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text"
+            value={formData.text}
+            onChange={(e) => setFormData({...formData, text: e.target.value})}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
